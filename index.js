9147c8f6af7e991201d8ab9a26183e6578dd645d
Lienien <116272320+Lienien@users.noreply.github.com>
2023-08-23 08:29:31 +0200

    Make code

---
+++ index.js
@@ -0,0 +1,41 @@
+// Oppgave 1
+// const oppg1 = window.prompt("Skriv")
+
+// function opp1(oppg1) {
+//     if (oppg1.length == 0){
+//         return "Teksten er tom"
+//     } else {
+//         return "Teksten er ikke tom"
+//     }
+// }
+
+// console.log(opp1(oppg1));
+
+// Oppgave 2
+// const oppg2 = ("Røyken Videregående Skole")
+
+// function opp2(setning) {
+//     return(setning.split(" "))
+// }
+
+// console.log(opp2(oppg2))
+
+// Oppgave 3
+
+// Oppgave 4
+const oppg4 = window.prompt("Skriv")
+
+function opp4(op4) {
+    return()
+}
+
+console.log(opp4(oppg4))
+// Oppgave 5
+
+// Oppgave 6
+var a = ("ABCAABCAAADA")
+function test() {
+    for (let i = 0; i < a.length; i+=3) {
+        console.log(substring)
+    }
+}
